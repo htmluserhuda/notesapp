e02ad7ac1b48f8e8a31edb259f43210a4b7928e2
huda elsayed <hdyalmsar@gmail.com>
2024-09-08 07:30:39 +0300

    noteitem

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/notes_view.dart';
 import 'package:notesapp/views/widgets/custom_appbar.dart';
+import 'package:notesapp/views/widgets/note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: const Column(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
         children: [
           SizedBox(
             height: 50,
           ),
           CustomAppbar(),
+          NoteItem(),
         ],
       ),
     );
