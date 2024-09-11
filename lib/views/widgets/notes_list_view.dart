ce2e32c1e630ea586fb4d0f6dc602b5c18308f66
huda elsayed <hdyalmsar@gmail.com>
2024-09-11 15:42:36 +0300

    floating_action_button

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -3,12 +3,13 @@ import 'package:notesapp/views/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
+  // final data = const [];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(itemBuilder: (context, index) {
       return const Padding(
-        padding: EdgeInsets.symmetric(vertical: 8.0),
+        padding: EdgeInsets.symmetric(vertical: 4.0),
         child: NoteItem(),
       );
     });
