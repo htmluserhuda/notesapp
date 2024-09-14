a7e7ef311da8e6a02acd4bf6fb2a6aa9f7d0d662
huda elsayed <hdyalmsar@gmail.com>
2024-09-14 23:06:14 +0300

    add fields to buttom sheat

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/views/widgets/constans.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxlines = 1});
+  final String hint;
+  final int maxlines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: KprimaryColor,
+      maxLines: maxlines,
       decoration: InputDecoration(
-        hintText: "Title",
+        hintText: hint,
         hintStyle: const TextStyle(color: KprimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
